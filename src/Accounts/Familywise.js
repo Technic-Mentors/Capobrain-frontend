3c43db131a987f55de0d62030abe3b0e115a485b
Ata422 <ataurrehmantechnicmentors@gmail.com>
2023-12-08 17:43:09 +0500

    first

--- src/Accounts/Familywise.js
+++ src/Accounts/Familywise.js
@@ -6,6 +6,7 @@ export default function Familywise() {
       <h1 style={{color:"#FAC400"}}>Family Wise Account</h1>
       <p>Because every student adds family details in their admission form and parent ID will be assigned at the end of student acceptance, the family wise account will be formed and it will reveal their kid account detail such as ID, name, class, section, and overall balance. This information is also available to parents via their portal.</p>
       <img src="/img/acc5.png" className='img-fluid' alt="" />
+      
     </div>
   )
 }
